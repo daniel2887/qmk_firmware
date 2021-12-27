c35f161e1d6f1cfe4fe03b846d6792eda3ca8fb1
Daniel <daniels2887@gmail.com>
2021-12-27 11:06:20 -0800

    Add RGB to Quefrency rev3 and rev4

--- keyboards/keebio/quefrency/keymaps/daniel2887/keymap.c
+++ keyboards/keebio/quefrency/keymaps/daniel2887/keymap.c
@@ -19,6 +19,8 @@ enum custom_keycodes {
 // To toggle between normal and gaming mode: Fn + Smiley
 // Otherwise, the keyboard should feel exactly the same (layer 1 ~ layer 4, layer 2 ~ layer 5)
 
+#define GAMING_LAYER_NUM 3
+
 bool encoder_update_user(uint8_t index, bool clockwise) {
 	// Only left encoder supported
 	if (index != 0) {
@@ -40,4 +42,30 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 	}
 	
     return true;
+}
+
+// Turn off LEDs when normal (non-gaming) layers are enabled.
+const rgblight_segment_t PROGMEM normal_layers_leds[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLED_NUM, HSV_BLACK}       // Turn off all RGBLED_NUM LEDs, starting with LED 0
+);
+
+// Light up all LEDs when gaming layers are enabled.
+const rgblight_segment_t PROGMEM gaming_layers_leds[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLED_NUM, HSV_WHITE}       // Light all RGBLED_NUM LEDs, starting with LED 0
+);
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    normal_layers_leds,
+    gaming_layers_leds
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+	rgblight_set_layer_state(0, 1);
+}
+
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(1, layer_state_cmp(state, GAMING_LAYER_NUM));
+    return state;
 }
