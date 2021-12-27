657048c1d18ec4001f8fffbb078f3a1f797aea6b
Daniel <daniels2887@gmail.com>
2021-12-27 11:10:27 -0800

    Add Quefrency update scripts

---
+++ scripts/update_quefrency_rev3.sh
@@ -0,0 +1,12 @@
+#/usr/bin/bash
+set -e
+
+cd qmk_firmware
+
+echo Updating keymap from json
+cd keyboards/keebio/quefrency/keymaps/daniel2887/
+qmk json2c *.json | sed 's/#include.*//g' > daniel2887_keymap.h
+cd -
+
+echo Compiling and Flashing
+qmk flash -j4 -kb keebio/quefrency/rev3 -km daniel2887
