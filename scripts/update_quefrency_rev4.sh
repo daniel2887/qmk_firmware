e37335c4d13b84a7b39534c7ff1977319cf266d1
Daniel <daniels2887@gmail.com>
2021-12-27 13:22:51 -0800

    Update quefrency scripts paths

--- scripts/update_quefrency_rev4.sh
+++ scripts/update_quefrency_rev4.sh
@@ -4,7 +4,7 @@ set -e
 cd qmk_firmware
 
 echo Updating keymap from json
-cd keyboards/keebio/quefrency/keymaps/daniel2887/
+cd ../keyboards/keebio/quefrency/keymaps/daniel2887/
 qmk json2c *.json | sed 's/#include.*//g' > daniel2887_keymap.h
 cd -
 
