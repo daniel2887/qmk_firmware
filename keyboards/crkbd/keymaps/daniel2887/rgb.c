530fc5efd5939948b53e63265f8fc97cb0fc9314
daniel2887 <daniels2887@gmail.com>
2023-07-24 23:38:37 -0700

    Corne: add RGB settings for arrows layer, adjust LED strengths, turn LEDs off by default

--- keyboards/crkbd/keymaps/daniel2887/rgb.c
+++ keyboards/crkbd/keymaps/daniel2887/rgb.c
@@ -52,12 +52,17 @@ bool rgb_process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 static void default_layer_rgb_set_normal(void) {
 	rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-	rgblight_sethsv_noeeprom(127, 0, 90);
+	rgblight_sethsv_noeeprom(127, 0, 20);
 }
 
 static void default_layer_rgb_set_gaming(void) {
 	rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-	rgblight_sethsv_noeeprom(85, 255, 110);
+	rgblight_sethsv_noeeprom(85, 255, 20);
+}
+
+static void default_layer_rgb_set_arrows(void) {
+	rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+	rgblight_sethsv_noeeprom(202, 95, 20);
 }
 
 static void default_layer_rgb_set_unknown(void) {
@@ -66,12 +71,16 @@ static void default_layer_rgb_set_unknown(void) {
 }
 
 void rgb_keyboard_post_init_user(void) {
+	// LEDs off by default
+	rgblight_disable_noeeprom();
 	default_layer_rgb_set_normal();
 }
 
 layer_state_t rgb_default_layer_state_set_user(layer_state_t state) {
 	if (layer_state_cmp(state, L_GAME)) {
 		default_layer_rgb_set_gaming();
+	} else if (layer_state_cmp(state, L_ARROWS)) {
+		default_layer_rgb_set_arrows();
 	} else if (layer_state_cmp(state, L_BASE)) {
 		default_layer_rgb_set_normal();
 	} else {
