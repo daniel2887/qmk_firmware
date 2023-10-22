d09c018a13e030e02709d65b48f22cf7ce7b6958
daniel2887 <daniels2887@gmail.com>
2023-10-21 18:34:49 -0700

    TPS42: disable OLED to reduce firmware size

--- keyboards/tps42/keymaps/daniel2887/rules.mk
+++ keyboards/tps42/keymaps/daniel2887/rules.mk
@@ -1,7 +1,7 @@
 SRC += ./tap_dance.c
 SRC += ./tap_hold.c
 
-OLED_ENABLE = yes				# uncomment if you are using an OLED display
+OLED_ENABLE = no				# uncomment if you are using an OLED display
 
 PS2_MOUSE_ENABLE = yes			# uncomment only on the master side if you are usin a TrackPoint
 MOUSEKEY_ENABLE = yes
